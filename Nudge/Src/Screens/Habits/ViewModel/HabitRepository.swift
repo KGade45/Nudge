6f382799e29760bb5b487feedc89f873010c88ed
Kaustubh Gade <koustubhgade@gmail.com>
2025-12-21 16:00:43 +0530

    Refactored extension dir

--- Nudge/Src/Screens/Habits/ViewModel/HabitRepository.swift
+++ Nudge/Src/Screens/Habits/ViewModel/HabitRepository.swift
@@ -8,34 +8,6 @@
 internal import CoreData
 import Foundation
 
-extension Habit {
-
-    var toHabitModel: HabitModel {
-        HabitModel(
-            id: self.id ?? {
-                assertionFailure("Habit id should never be nil")
-                return UUID()
-            }(),
-            title: self.title ?? "",
-            createdAt: self.createdAt ?? Date(),
-            isActive: self.isActive,
-            preferredStartHour: Int(self.preferredStartHour),
-            preferredEndHour: Int(self.preferredEndHour),
-            lastCompletedAt: self.lastCompletedAt)
-    }
-    /// Convenience initializer to map from HabitModel to Core Data Habit
-    convenience init(from model: HabitModel, context: NSManagedObjectContext) {
-        self.init(context: context)
-        self.id = model.id
-        self.title = model.title
-        self.createdAt = model.createdAt
-        self.isActive = model.isActive
-        self.lastCompletedAt = model.lastCompletedAt
-        self.preferredEndHour = Int16(model.preferredEndHour)
-        self.preferredStartHour = Int16(model.preferredStartHour)
-    }
-}
-
 final class HabitRepository {
 
     private let context: NSManagedObjectContext
